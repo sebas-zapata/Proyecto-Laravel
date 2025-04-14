6e0dcac5c67121c8c8dfcb14cf6ed7d7dd9d93da
Sebastian Zapata <zapatajuan351@gmail.com>
2025-04-14 11:15:00 -0500

    Ajustes al proyecto

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,3 @@
-import './bootstrap';
 import Alpine from 'alpinejs';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 window.Alpine = Alpine;
 Alpine.start();
